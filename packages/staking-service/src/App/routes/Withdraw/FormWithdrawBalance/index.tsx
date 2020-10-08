f3ab936969916c5d9691903590bd00564c5948b2
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-10-08 09:55:57 +0200

    Add logic to Withdraw

--- packages/staking-service/src/App/routes/Withdraw/FormWithdrawBalance/index.tsx
+++ packages/staking-service/src/App/routes/Withdraw/FormWithdrawBalance/index.tsx
@@ -3,18 +3,52 @@ import { Formik } from "formik";
 import { Form, FormItem, Input } from "formik-antd";
 import React from "react";
 import { FormField, FormStack } from "./style";
+import { StakingValidator } from "../../../utils/staking";
+import { useAccount, useSdk, displayAmountToNative } from "@cosmicdapp/logic";
+import { config } from "../../../../config";
+import { Coin, coins } from "@cosmjs/launchpad";
+import { MsgUndelegate } from "@cosmjs/launchpad/types/msgs";
 
 const { Text } = Typography;
 
-export interface FormWithdrawBalanceFields {
+interface FormWithdrawBalanceFields {
   readonly amount: string;
 }
 
 interface FormWithdrawBalanceProps {
-  readonly submitWithdrawBalance: (values: unknown) => void;
+  readonly validator: StakingValidator;
 }
 
-export function FormWithdrawBalance({ submitWithdrawBalance }: FormWithdrawBalanceProps): JSX.Element {
+export function FormWithdrawBalance({ validator }: FormWithdrawBalanceProps): JSX.Element {
+  const { account } = useAccount();
+  const { getClient } = useSdk();
+
+  function submitWithdrawBalance({ amount }: FormWithdrawBalanceFields) {
+    const nativeAmountString = displayAmountToNative(amount, config.coinMap, config.stakingToken);
+    const nativeAmountCoin: Coin = { amount: nativeAmountString, denom: config.stakingToken };
+
+    const delegateMsg: MsgUndelegate = {
+      type: "cosmos-sdk/MsgUndelegate",
+      value: {
+        delegator_address: account.address,
+        validator_address: validator.operator_address,
+        amount: nativeAmountCoin,
+      },
+    };
+
+    const defaultFee = {
+      amount: coins(37500, config.feeToken),
+      gas: "1500000", // 1.5 million
+    };
+
+    getClient()
+      .signAndBroadcast([delegateMsg], defaultFee)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch(console.error);
+  }
+
   return (
     <Formik initialValues={{ amount: "" }} onSubmit={submitWithdrawBalance}>
       {(formikProps) => (
--- packages/staking-service/src/App/routes/Withdraw/index.tsx
+++ packages/staking-service/src/App/routes/Withdraw/index.tsx
@@ -1,20 +1,21 @@
 import { PageLayout } from "@cosmicdapp/design";
 import { Typography } from "antd";
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { HeaderBackMenu } from "../../components/HeaderBackMenu";
-import { pathOperationResult } from "../../paths";
+import { useStakingValidator } from "../../utils/staking";
 import { FormWithdrawBalance } from "./FormWithdrawBalance";
 import { HeaderTitleStack, MainStack } from "./style";
 
 const { Title } = Typography;
 
-export function Withdraw(): JSX.Element {
-  const history = useHistory();
+interface WithdrawParams {
+  readonly validatorAddress: string;
+}
 
-  function submitWithdrawBalance() {
-    history.push(pathOperationResult);
-  }
+export function Withdraw(): JSX.Element {
+  const { validatorAddress } = useParams<WithdrawParams>();
+  const validator = useStakingValidator(validatorAddress);
 
   return (
     <PageLayout>
@@ -22,9 +23,9 @@ export function Withdraw(): JSX.Element {
         <HeaderTitleStack>
           <HeaderBackMenu />
           <Title>Withdraw</Title>
-          <Title level={2}>Iris Net</Title>
+          <Title level={2}>{validator?.description.moniker ?? ""}</Title>
         </HeaderTitleStack>
-        <FormWithdrawBalance submitWithdrawBalance={submitWithdrawBalance} />
+        <FormWithdrawBalance validator={validator} />
       </MainStack>
     </PageLayout>
   );
